09544edb512d33ff6fd4b8ff0068cdbc33b1a0dd
khaledkamr <200013171@must.edu.eg>
2023-09-18 12:50:36 +0300

    Added the solution of problem A of sheet 8

---
+++ Assit sheets/Sheet #8 (General easy)/A - Palindromes replace.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+#define ll long long
+using namespace std;
+
+int main()
+{
+	string str;
+	cin >> str;
+	int size = str.size() - 1;
+
+	for (int i = 0; i <= size; i++)
+	{
+		if (str[i] == '?')
+		{
+			if (str[size - i] != '?')
+			{
+				str[i] = str[size - i];
+			}
+			else if (str[size - i] == '?' || i == size - i)
+			{
+				str[i] = 'a';
+			}
+		}
+	}
+
+	string rev = str;
+	reverse(rev.begin(), rev.end());
+
+	if (str == rev)
+	{
+		cout << str;
+	}
+	else
+	{
+		cout << -1;
+	}
+}
