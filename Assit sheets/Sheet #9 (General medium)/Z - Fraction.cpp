1a100de1fb555f0120c26d70820acb87c493249d
khaledkamr <200013171@must.edu.eg>
2023-10-03 09:39:21 +0300

    modified the solution of the problem Z of sheet 9

--- Assit sheets/Sheet #9 (General medium)/Z - Fraction.cpp
+++ Assit sheets/Sheet #9 (General medium)/Z - Fraction.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cstdlib>
 #include<string>
+#include<numeric> 
 #include<algorithm>
 #include<math.h>
 #include<cmath>
@@ -10,35 +11,31 @@
 #define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(0);
 using namespace std;
 
+int GCD(int a, int b)
+{
+	if (b == 0) return a;
+
+	return GCD(b, a % b);
+}
+
+int LCM(int a, int b)
+{
+	return(a / GCD(a, b)) * b;
+}
+
 int main()
 {
 	IOS;
 	string f1, f2;
-	cin >> f1 >> f2; // 1/2  3/4
+	cin >> f1 >> f2; 
 
 	int pos = f1.find('/');
-	int num1 = stoi(f1.substr(pos - 1));
 	int den1 = stoi(f1.substr(pos + 1));
+	int num1 = stoi(f1.erase(pos));
 
 	pos = f2.find('/');
-	int num2 = stoi(f2.substr(pos - 1));
 	int den2 = stoi(f2.substr(pos + 1));
-
-	cout << max(num1, num2) << '/';
-
-	if (den1 % 2 != 0 || den2 % 2 != 0)
-	{
-		cout << 1;
-	}
-	else
-	{
-		for (int i = 2; i <= min(den1, den2); i++)
-		{
-			if (den1 % i == 0 && den2 % i == 0)
-			{
-				cout << i;
-				return 0;
-			}
-		}
-	}
+	int num2 = stoi(f2.erase(pos));
+	
+	cout << LCM(num1, num2) << '/' << GCD(den1, den2);
 }
