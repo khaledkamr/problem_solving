885a07bd6a03568d3f5eaed92ee9d31b8fe031e8
khaledkamr <200013171@must.edu.eg>
2023-10-06 10:14:12 +0300

    Modified the problem G

--- Assit sheets/Sheet #10 (General hard)/G - Smallest product.cpp
+++ Assit sheets/Sheet #10 (General hard)/G - Smallest product.cpp
@@ -24,19 +24,10 @@ int main()
 		cin >> vec.at(i);
 	}
 
-<<<<<<< HEAD
     for(it = vec.begin(); it < vec.end(); it++)
     {
         log_product += log10(*it);
     }
 
 	cout << (ll)ceil(pow(10, log_product / n));
-=======
-        for(it = vec.begin(); it < vec.end(); it++)
-        {
-                product += log10(*it);
-        }
-
-	cout << (ll)ceil(pow(10, product / n));
->>>>>>> 5c46225cdcc3988ed0fd743bd45ebbe47865a421
 }
