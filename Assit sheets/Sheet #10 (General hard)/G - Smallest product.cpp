bd0110018282028ee0a4cf55096936f3bfcab802
khaledkamr <200013171@must.edu.eg>
2023-10-06 09:47:33 +0300

    Added the solution of problem G of sheet 10

---
+++ Assit sheets/Sheet #10 (General hard)/G - Smallest product.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<numeric> 
+#include<algorithm>
+#include<math.h>
+#include<cmath>
+#include<vector>
+#include<utility>
+#define ll long long
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(0);
+using namespace std;
+
+int main()
+{
+	IOS;
+	double n, product = 0;
+	cin >> n;
+	vector<ll> vec(n);
+	vector<ll>::iterator it;
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> vec.at(i);
+		product += log10(vec.at(i));
+	}
+
+	cout << (ll)ceil(pow(10, product / n));
+}
