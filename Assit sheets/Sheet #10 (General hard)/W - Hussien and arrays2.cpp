bb5f370674e1ae85d6c5fbb90ee4a1babae85964
khaledkamr <200013171@must.edu.eg>
2023-10-30 21:29:15 +0200

    Modified the sloution

--- Assit sheets/Sheet #10 (General hard)/W - Hussien and arrays2.cpp
+++ Assit sheets/Sheet #10 (General hard)/W - Hussien and arrays2.cpp
@@ -13,12 +13,17 @@ int main()
 	cin>>size;
 	int arr[size];
 	priority_queue<pair<int,int> > p;
-	for(int i=0;i<size;i++){
+
+	for(int i=0;i<size;i++)
+    {
 		cin>>arr[i];
 		p.push({arr[i],i});
 	}
-	for(int i=0;i<size;i++){
-		while(p.size() > 0 && p.top().first >= arr[i]){
+    
+	for(int i=0;i<size;i++)
+    {
+		while(p.size() > 0 && p.top().first >= arr[i])
+        {
 			res = max(res,p.top().second - i);
 			p.pop();
 		}
