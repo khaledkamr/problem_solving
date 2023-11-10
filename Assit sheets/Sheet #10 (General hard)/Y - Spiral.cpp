53b53dfc4b585b72ae75f73a4c0dbe5af99e2d4c
khaledkamr <200013171@must.edu.eg>
2023-11-10 17:28:56 +0200

    Added the solution of problem Y of sheet 10

---
+++ Assit sheets/Sheet #10 (General hard)/Y - Spiral.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<cstdlib>
+#include<string>
+#include<numeric> 
+#include<algorithm>
+#include<math.h>
+#include<cmath>
+#include<iomanip>
+#include<vector>
+#include<utility>
+#include<unordered_set>
+#include<climits>
+#define ll long long
+#define IOS ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(0);
+using namespace std;
+
+int main()
+{ 
+	IOS;
+	ll row, col;
+	cin >> row >> col;
+	ll matrix[row][col];
+
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++)
+		{
+			cin >> matrix[i][j];
+		}
+	}
+
+	int r = 0, c = 0;
+
+	while(r < row && c < col)
+	{
+		for(int i = c; i < col; i++)
+		{
+			cout << matrix[r][i] << " ";
+		}
+
+		r++;
+
+		for(int i = r; i < row; i++)
+		{
+			cout<< matrix[i][col -1] << " ";
+		}
+
+		col--;
+
+		if(r < row)
+		{
+			for(int i = col - 1; i >= c; i--)
+			{
+				cout << matrix[row - 1][i] << " ";
+			}
+			row--;
+		}
+		if(c < col)
+		{
+			for(int i = row - 1; i >= r; i--)
+			{
+				cout << matrix[i][c] << " ";
+			}
+			c++;
+		}
+	} 
+}
