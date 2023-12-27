80b0878a3e376d197e24d7af4b44f7f31d3e0061
khaledkamr <200013171@must.edu.eg>
2023-12-27 18:29:37 +0200

    Added the solution of problem A

---
+++ Assit sheets/Sheet #4 (Strings)/A - Create a new string.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+	string s, t;
+	cin >> s >> t;
+
+	cout << s.length() << " " << t.length() << endl;
+	cout << s << " " << t;
+
+	return 0;
+}
