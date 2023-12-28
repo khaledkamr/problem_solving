a160bb2a5bf7f21fd8132a8d693aa50800a83a84
khaledkamr <200013171@must.edu.eg>
2023-12-28 14:29:54 +0200

    solution of problem B

---
+++ Assit sheets/Sheet #4 (Strings)/B - Let's use getline.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+#include<algorithm>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+	string w;
+	getline(cin, w, '\\');
+
+	cout << w;
+
+	return 0;
+}
